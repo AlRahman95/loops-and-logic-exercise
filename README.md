c4604b7e49da444e73e2e23cb54f025f4bf9b0dc
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-15 20:56:26 -0500

    added readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Loops and Logic exercise
+
+Worked on logic using the if, if else, and else if statements, and loops using while and for loops. Then we worked on exercises to apply loops and logic such as through the palindrome exercise. 
--- js/exercise.js
+++ js/exercise.js
@@ -1,34 +1,34 @@
 //Log every number from 0 to 100, including 0 and 100, but if the number is evenly divisible by 3, log 'fizz' instead, if the number is evenly divisible by 5 log 'buzz' instead, if the number is evenly divisible by 3 and 5 log 'fizzbuzz', if it is not evenly divisible by either 3 or 5 log the number itself
 
-//1st way
-// for (var x = 0; x <= 100; x++) {
-//   if (x === 0) {
-//     console.log(0);
-//   } else if(x % 3 === 0 && x % 5 === 0) {
-//     console.log("fizzbuzz");
-//   } else if (x % 3 === 0) {
-//     console.log("fizz");
-//   } else if (x % 5 === 0) {
-//     console.log("buzz");
-//   } else {
-//     console.log(x)
-//   }
-// }
+// 1st way
+for (var x = 0; x <= 100; x++) {
+  if (x === 0) {
+    console.log(0);
+  } else if(x % 3 === 0 && x % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (x % 3 === 0) {
+    console.log("fizz");
+  } else if (x % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(x)
+  }
+}
 
-//2nd way
-// for (var x = 0; x <= 100; x++) {
-//   if (x === 0) {
-//     console.log(0);
-//   } else if (x % 3 === 0 && !(x % 5 === 0)) {
-//     console.log("fizz");
-//   } else if (x % 5 === 0 && !(x % 3 ===0)) {
-//     console.log("buzz");
-//   } else if (x % 3 === 0 && x % 5 === 0) {
-//     console.log("fizzbuzz");
-//   } else {
-//     console.log(x);
-//   }
-// }
+// 2nd way
+for (var x = 0; x <= 100; x++) {
+  if (x === 0) {
+    console.log(0);
+  } else if (x % 3 === 0 && !(x % 5 === 0)) {
+    console.log("fizz");
+  } else if (x % 5 === 0 && !(x % 3 ===0)) {
+    console.log("buzz");
+  } else if (x % 3 === 0 && x % 5 === 0) {
+    console.log("fizzbuzz");
+  } else {
+    console.log(x);
+  }
+}
 
 
 //.split, .reverse are methods for strings and .join is a method for an array
@@ -45,20 +45,18 @@ myArr[myArr.length - 1];
 
 //Create a function that tests if a string is a palindrome
 
-//1st way
-// function checkForPalindrome(str) {
-//   var tester = str;
-//   tester = tester.split("");
-//   tester = tester.reverse();
-//   tester = tester.join("");
-//   if (tester === str) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-//
-// checkForPalindrome("lila");
+// 1st way
+function checkForPalindrome(str) {
+  var tester = str;
+  tester = tester.split("");
+  tester = tester.reverse();
+  tester = tester.join("");
+  if (tester === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 //2nd way
@@ -66,10 +64,12 @@ function checkForPalindrome(str) {
   var test = str;
   test = test.split("");
   var y = test.length;
-
   for (var x = 0; x < test.length; x++) {
     y--;
     if (test[x] === test[y]) {
+      // if (x === 0) {
+      //   break;
+      // }
       return true;
     } else {
       return false;
